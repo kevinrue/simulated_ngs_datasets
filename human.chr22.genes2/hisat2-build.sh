679d18e738bc8b8d7db12ce7ea86ddc597cf5585
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2021-11-28 14:00:55 +0000

    remove unnecessary cd statement

--- human.chr22.genes2/hisat2-build.sh
+++ human.chr22.genes2/hisat2-build.sh
@@ -1,5 +1,4 @@
 conda activate hisat2
-cd human.chr22.genes2
 mkdir downloads
 mkdir outputs
 wget http://ftp.ensembl.org/pub/release-104/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.22.fa.gz -P downloads
