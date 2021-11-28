18be810d977879ce9ac9c6bf7b22796033cc1b79
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2021-11-28 13:49:25 +0000

    add script converting FASTQ files to FASTA format

---
+++ human.chr22.genes2/bioconvert.fasta2fastq.sh
@@ -0,0 +1,5 @@
+conda activate hisat2
+bioconvert outputs/simulated_reads/sample_01_1.fasta outputs/simulated_reads/sample_01_1.fastq
+bioconvert outputs/simulated_reads/sample_01_2.fasta outputs/simulated_reads/sample_01_2.fastq
+bioconvert outputs/simulated_reads/sample_02_1.fasta outputs/simulated_reads/sample_02_1.fastq
+bioconvert outputs/simulated_reads/sample_02_2.fasta outputs/simulated_reads/sample_02_2.fastq
