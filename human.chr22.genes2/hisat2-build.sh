30d0fad6148427b241bf2ff36bf6810270f0ca03
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2021-11-28 11:48:37 +0000

    force shell script to create output directory

--- human.chr22.genes2/hisat2-build.sh
+++ human.chr22.genes2/hisat2-build.sh
@@ -1,7 +1,8 @@
 conda activate hisat2
 cd human.chr22.genes2
 mkdir downloads
-# wget http://ftp.ensembl.org/pub/release-104/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.22.fa.gz -P downloads
+mkdir outputs
+wget http://ftp.ensembl.org/pub/release-104/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.22.fa.gz -P downloads
 gzip -d downloads/Homo_sapiens.GRCh38.dna.chromosome.22.fa.gz
 hisat2-build downloads/Homo_sapiens.GRCh38.dna.chromosome.22.fa outputs/hisat2_chr22
 
